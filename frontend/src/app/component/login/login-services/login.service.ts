8c568e502914e6a9f846f818101c70f9ad7c1975
weguilherme <dguilherme946@gmail.com>
2023-11-04 14:54:18 -0300

    configuracao de producao

--- frontend/src/app/component/login/login-services/login.service.ts
+++ frontend/src/app/component/login/login-services/login.service.ts
@@ -13,7 +13,7 @@ import { ExceptionsService } from '../../exception/exception-services/exceptions
 })
 export class LoginService {
 
-  baseUrl = '/api/'
+  baseUrl = 'https://api-spring-book.onrender.com/'
 
   private httpClient: HttpClient;
 
